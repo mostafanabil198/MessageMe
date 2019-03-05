17ce16e52351f6bf60e19db635807f49aadc73be
mostafa <mostafanabil198@gmail.com>
2019-03-06 00:01:54 +0200

    test2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.web_socket_server_url = "https://pawns-messageme.herokuapp.com/cable"
+  config.web_socket_server_url = "wss://pawns-messageme.herokuapp.com/cable"
   config.action_cable.allowed_request_origins = ['https://pawns-messageme.herokuapp.com', 'http://pawns-messageme.herokuapp.com']
-  
+
 end
